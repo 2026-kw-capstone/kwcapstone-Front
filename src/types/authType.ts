c52d4ed3bf379bdd706337a5b6d0591e864a0f22
cwyoung02 <cwyoung722@gmail.com>
2026-03-24 15:42:27 +0900

    Feat:회원가입 API 연결

--- src/types/authType.ts
+++ src/types/authType.ts
@@ -2,33 +2,34 @@ export type CommonResponse<T> = {
   status: number;
   message: string;
   data: T;
-}
+};
 
-// 회원가입
 export type RequestSignupDto = {
   email: string;
   password: string;
-  name: string;
-}
+  nickname: string;
+};
 
-export type ResponseSignupDto = CommonResponse<{
-    id: number | string;
+export type ResponseSignupDto = {
+  isSuccess: boolean;
+  code: string;
+  message: string;
+  result: {
+    memberId: number;
     email: string;
-    name: string;
-}>
+    nickname: string;
+  };
+};
 
-// 로그인
 export type RequestSigninDto = {
   email: string;
   password: string;
-}
+};
 
 export type ResponseSigninDto = CommonResponse<{
-    accessToken: string;
-    // refreshToken?: string;
+  accessToken: string;
 }>;
 
-// 액세스 토큰 재발급
 export type ResponseReissueDto = {
   isSuccess: boolean;
   code: string;
@@ -38,14 +39,12 @@ export type ResponseReissueDto = {
   };
 };
 
-// 로그아웃
 export type ResponseSignoutDto = CommonResponse<{
-    success: boolean;
+  success: boolean;
 }>;
 
-// 내 정보 조회
 export type ResponseMyInfoDto = CommonResponse<{
-    id: number | string;
-    email: string;
-    name: string;
+  id: number | string;
+  email: string;
+  name: string;
 }>;
